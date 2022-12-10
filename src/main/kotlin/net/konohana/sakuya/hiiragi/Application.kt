51261c7d3ee2374b22bd0c72db078ccfd29f3bb0
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-12-10 13:49:29 +0900

    20221210 DB接続処理追加

--- src/main/kotlin/net/konohana/sakuya/hiiragi/Application.kt
+++ src/main/kotlin/net/konohana/sakuya/hiiragi/Application.kt
@@ -1,6 +1,7 @@
 package net.konohana.sakuya.hiiragi
 
 import io.ktor.server.application.Application
+import net.konohana.sakuya.hiiragi.db.initHiiragiDB
 import net.konohana.sakuya.hiiragi.plugins.configureHTTP
 import net.konohana.sakuya.hiiragi.plugins.configureRouting
 import net.konohana.sakuya.hiiragi.plugins.configureSerialization
@@ -10,6 +11,10 @@ fun main(args: Array<String>): Unit =
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    val url = environment.config.property("ktor.hiiragidb.uri").getString()
+    val username = environment.config.property("ktor.hiiragidb.username").getString()
+    val password = environment.config.property("ktor.hiiragidb.password").getString()
+    initHiiragiDB(url, username, password)
     configureSerialization()
     configureHTTP()
     configureRouting()
